b210583c2401ecf50d018d53fb1b32356a412f56
Jerome Viveret <jerome.viveret@gmail.com>
2021-11-25 18:06:20 +0100

    Add allocAndEncode(), seekKey(), slice()

--- src/lib.zig
+++ src/lib.zig
@@ -4,11 +4,18 @@ const c = @import("c.zig");
 const helpers = @import("helpers.zig");
 const enc = @import("encode.zig");
 const dec = @import("decode.zig");
+const seek_key = @import("seek_key.zig");
+
+var alloc = std.heap.GeneralPurposeAllocator(.{}){};
+var allocator = &alloc.allocator;
 
 export fn napi_register_module_v1(env: c.napi_env, exports: c.napi_value) c.napi_value {
     helpers.register_function(env, exports, "encodingLength", encodingLength) catch return null;
     helpers.register_function(env, exports, "encode", encode) catch return null;
     helpers.register_function(env, exports, "decode", decode) catch return null;
+    helpers.register_function(env, exports, "seekKey", seekKey) catch return null;
+    helpers.register_function(env, exports, "allocAndEncode", allocAndEncode) catch return null;
+    helpers.register_function(env, exports, "slice", slice) catch return null;
     return exports;
 }
 
@@ -78,4 +85,82 @@ fn decode(env: c.napi_env, info: c.napi_callback_info) callconv(.C) c.napi_value
     }
 
     return dec.decode(env, buffer, start) catch return null;
+}
+
+fn allocAndEncode(env: c.napi_env, info: c.napi_callback_info) callconv(.C) c.napi_value {
+    var argc: usize = 1;
+    var argv: [1]c.napi_value = undefined;
+    if (c.napi_get_cb_info(env, info, &argc, &argv, null, null) != .napi_ok) {
+        helpers.throw(env, "Failed to get args") catch return null;
+    }
+    if (argc < 1) {
+        helpers.throw(env, "Not enough arguments") catch return null;
+    }
+
+    var inputJS = argv[0];
+
+    const len = enc.encodingLength(env, inputJS) catch 0;
+
+    var dest = allocator.alloc(u8, len) catch return null;
+    defer allocator.free(dest);
+
+    var written = enc.encode(env, inputJS, dest, 0) catch 0;
+    return helpers.create_buffer(env, dest, "could not create buffer") catch return null;
+}
+
+fn seekKey(env: c.napi_env, info: c.napi_callback_info) callconv(.C) c.napi_value {
+    var argc: usize = 3;
+    var argv: [3]c.napi_value = undefined;
+    if (c.napi_get_cb_info(env, info, &argc, &argv, null, null) != .napi_ok) {
+        helpers.throw(env, "Failed to get args") catch return null;
+    }
+    if (argc < 3) {
+        helpers.throw(env, "Not enough arguments") catch return null;
+    }
+
+    var buffer = helpers.slice_from_value(env, argv[0], "1st arg") catch return null;
+    var start_i: i32 = undefined;
+    if (c.napi_get_value_int32(env, argv[1], &start_i) != .napi_ok) {
+        helpers.throw(env, "Failed to get start") catch return null;
+    }
+
+    if (start_i == -1) {
+        return helpers.i32ToJS(env, -1) catch return null;
+    }
+
+    var key: []u8 = undefined;
+    if (helpers.isTypeof(env, argv[2], c.napi_valuetype.napi_string)) {
+        // TODO document / modify bound
+        var len: usize = 2048;
+        var bytes: usize = undefined;
+        if (c.napi_get_value_string_utf8(env, argv[2], @ptrCast([*c]u8, key), len, bytes) != .napi_ok) {
+            helpers.throw(env, "3rd arg is not a string") catch return null;
+        }
+    } else {
+        key = helpers.slice_from_value(env, argv[2], "3rd arg") catch return null;
+    }
+    if (seek_key.seekKey(env, buffer, @intCast(u32, start_i), key)) |result| {
+        return helpers.u32ToJS(env, result) catch return null;
+    } else |err| switch(err) {
+        error.NOTFOUND => return helpers.i32ToJS(env, -1) catch return null
+    }
+}
+
+fn slice(env: c.napi_env, info: c.napi_callback_info) callconv(.C) c.napi_value {
+    var argc: usize = 2;
+    var argv: [2]c.napi_value = undefined;
+    if (c.napi_get_cb_info(env, info, &argc, &argv, null, null) != .napi_ok) {
+        helpers.throw(env, "Failed to get args") catch return null;
+    }
+    if (argc < 2) {
+        helpers.throw(env, "Not enough arguments") catch return null;
+    }
+    var buffer = helpers.slice_from_value(env, argv[0], "1st arg") catch return null;
+    var start: u32 = undefined;
+    if (c.napi_get_value_uint32(env, argv[1], &start) != .napi_ok) {
+        helpers.throw(env, "Failed to get start") catch return null;
+    }
+
+    var res = seek_key.slice(env, buffer, start) catch return null;
+    return helpers.create_buffer(env, res, "could not create buffer")catch return null;
 }
---
+++ src/seek_key.zig
@@ -0,0 +1,49 @@
+const std = @import("std");
+const c = @import("c.zig");
+const varint = @import("varint.zig");
+const constants = @import("constants.zig");
+
+pub const SEEK_ERROR = error {
+    NOTFOUND
+};
+
+pub fn seekKey(env: c.napi_env, buffer: []u8, start: u32, target: []u8) SEEK_ERROR!u32 {
+    var tag = varint.decode(buffer, start) catch return error.NOTFOUND;
+    var typ = tag.res & constants.TAG_MASK;
+
+    var target_length = target.len;
+    var len = tag.res >> constants.TAG_SIZE;
+    var i = tag.bytes;
+
+    while(i < len) {
+        var key_tag = varint.decode(buffer, start + i) catch return error.NOTFOUND;
+        i += key_tag.bytes;
+        var key_len = key_tag.res >> constants.TAG_SIZE;
+        var key_type = key_tag.res & constants.TAG_MASK;
+        if (key_type == constants.STRING and target_length == key_len) {
+            for (target) |u, j| {
+                if (buffer[start + i + j] != u) {
+                    break;
+                }
+                if (j == target_length - 1) {
+                    return start + i + key_len;
+                }                
+            }
+        }
+
+        i += key_len;
+        var value_tag = varint.decode(buffer, start + i) catch return error.NOTFOUND;
+        i += value_tag.bytes;
+        var value_len = value_tag.res >> constants.TAG_SIZE;
+        i += value_len;
+    }
+
+    return error.NOTFOUND;
+}
+
+pub fn slice(env: c.napi_env, buffer: []u8, start: u32) SEEK_ERROR![]u8 {
+    var tag = varint.decode(buffer, start) catch return error.NOTFOUND;
+    var new_start = start + tag.bytes;
+    
+    return buffer[new_start..new_start + (tag.res >> constants.TAG_SIZE)];
+}
